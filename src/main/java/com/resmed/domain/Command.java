b0d7dd5ff2e23696673b3345a1d8dc8ee349684b
hemangi <hemangi.khairnar@gmail.com>
2020-03-19 15:58:08 +1100

    remove unused commands

--- src/main/java/com/resmed/domain/Command.java
+++ src/main/java/com/resmed/domain/Command.java
@@ -1,11 +1,9 @@
 package com.resmed.domain;
 
 public enum Command {
-    P("Place"),
     M("Move"),
     L("Left"),
-    R("Right"),
-    C("Report");
+    R("Right");
 
     private String comm;
     Command(String comm) {
